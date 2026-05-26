ae0a09f8e1b4bcd3b4f6fae3fee1042b72d45a4d
Marsalys GBEGONNOUDE <marsalys1@gmail.com>
2026-05-26 13:15:11 +0100

    fix: ip all

--- python-web-app/devops/settings.py
+++ python-web-app/devops/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0)a&brejfivgamvhleqx7)__8pqxpt5z^w2jrurkgz$*f*dgrx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
